b873413db62d9cbb6b754a163920cde689d53b2c
mitchilling <8478982+mitchilling@users.noreply.github.com>
2025-08-19 23:23:08 +0800

    `step-11`: Enable vertical swiping and update song titles, and add an `InfoBox` component to display them

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -1,4 +1,5 @@
 import { ActionBar } from './ActionBar.jsx';
+import { InfoBox } from './InfoBox.js';
 import { ProgressBar } from './ProgressBar.jsx';
 import { VinylRecord } from './VinylRecord.jsx';
 
@@ -8,6 +9,7 @@ export const SongItem = (props: { index: number }) => {
       style={{ width: "100%", height: "100vh", display: "flex", flexDirection: "column" }}
     >
       <VinylRecord name={`song-${props.index}`} />
+      <InfoBox />
       <ProgressBar />
       <ActionBar />
     </view>
--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,6 +1,18 @@
+import type { ListSnapEvent } from '@lynx-js/types';
+import { useAtom } from 'jotai';
+import { currentTitleAtom, managerAtom } from '../State.jsx';
 import { SongItem } from './SongItem.jsx'
 
 export const SongList = () => {
+  const [, setCurrentTitle] = useAtom(currentTitleAtom);
+  const [manager, ] = useAtom(managerAtom);
+
+  // triggered when the finger leaves the screen after a swipe
+  const handleSnap = (e: ListSnapEvent) => {
+    manager?.switchTo(e.detail.position);
+
+    setCurrentTitle(manager?.currentSong?.title ?? null);
+  };
   return (
     <list
       scroll-orientation="vertical"
@@ -8,12 +20,15 @@ export const SongList = () => {
       span-count={1}
       // use item-snap to achieve pagination
       item-snap={{ factor: 0, offset: 0 }}
+      // bug with bindsnap, let's ignore it
+      // @ts-ignore
+      bindsnap={handleSnap}
       style={{
         width: "100%",
         height: "100vh",
       }}
     >
-      {Array.from({ length: 3 }).map((item, index) => {
+      {Array.from({ length: manager?.currentPositions.length ?? 0 }).map((item, index) => {
         return (
           <list-item
             item-key={`list-item-${index}`}
