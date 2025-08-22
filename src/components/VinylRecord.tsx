50ae097e53be634a49f915fbee4c6c082784cc61
mitchilling <8478982+mitchilling@users.noreply.github.com>
2025-08-22 19:39:35 +0800

    `step-15.5`: Prepare for spinning vinyl animation

--- src/components/VinylRecord.tsx
+++ src/components/VinylRecord.tsx
@@ -1,7 +1,12 @@
+import { useAtom } from 'jotai';
+import { isPlayingAtom } from '../State.jsx';
+
 import './VinylRecord.css'
 import vinyl from '../assets/images/vinyl_red.png';
 
-export const VinylRecord = (props: { name: string }) => {
+export const VinylRecord = () => {
+  const [isPlaying, ] = useAtom(isPlayingAtom);
+
   return (
     <view className='VinylRecordView'>
       <image src={vinyl} className='VinylImage' />
